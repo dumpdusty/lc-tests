29d83a5522a17d7b7d41268849627b4a48fd78e6
dumpdusty <dumpdusty@gmail.com>
2022-08-04 00:25:04 -0700

    add tests for warning messages

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -23,20 +23,49 @@ describe('Authentication', function () {
 
     describe('Negative Tests', function () {
 
-        it('Sign in with invalid credentials', function () {
+        it.skip('Check the toaster', function () {
 
             cy.visit('/user/login')
-
             cy.get('#normal_login_email')
                 .type('dumpdusty2@gmail.com')
             cy.get('#normal_login_password')
                 .type('test')
             cy.get('.login-form-button')
                 .click()
-
-
             cy.get('.ant-notification-notice-message')
                 .should('have.text', 'Auth failed')
+
+            cy.visit('/user/login')
+            cy.get('#normal_login_email')
+                .type('test@test.test')
+            cy.get('#normal_login_password')
+                .type('Qwerty123')
+            cy.get('.login-form-button')
+                .click()
+            cy.get('.ant-notification-notice-message')
+                .should('have.text', 'Auth failed')
+        });
+
+        it('Check the warning messages', function () {
+
+            cy.visit('/user/login')
+            cy.get('#normal_login_email')
+                .type('test')
+            cy.get('//div[contains(@class, \'ant-form-item-has-error\')][.//input[@id=\'normal_login_email\']]//div[@class=\'ant-form-item-explain-error\']')
+                .should('have.text', '\'email\' is not a valid email')
+
+            // cy.get('#normal_login_email')
+            //     .type('test')
+            //     .clear()
+            // cy.get('')
+            //     .should('have.text', 'Required')
+            //
+            // cy.get('#normal_login_password')
+            //     .type('test')
+            // cy.get('')
+            //     .should('have.text', '')
+
+
         });
     })
 

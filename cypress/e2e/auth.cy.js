6627f35231e32af664b1803ac74bc057ad575c7c
dumpdusty <dumpdusty@gmail.com>
2022-08-03 22:33:33 -0700

    add invalid password test

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,20 +1,43 @@
 describe('Authentication', function () {
-    it('Sign in with valid credentials', function () {
 
-        cy.visit('/user/login')
+    describe.skip('Positive tests', function () {
 
-        cy.get('#normal_login_email')
-            .type('dumpdusty2@gmail.com')
-        cy.get('#normal_login_password')
-            .type('Qwerty123')
-        cy.get('.login-form-button')
-            .click()
+        it('Sign in with valid credentials', function () {
 
-        cy.get('.ant-avatar-square')
-            .should('be.visible')
-        cy.location('pathname')
-            .should('include', 'profile')
+            cy.visit('/user/login')
 
+            cy.get('#normal_login_email')
+                .type('dumpdusty2@gmail.com')
+            cy.get('#normal_login_password')
+                .type('Qwerty123')
+            cy.get('.login-form-button')
+                .click()
+
+            cy.get('.ant-avatar-square')
+                .should('be.visible')
+            cy.location('pathname')
+                .should('include', 'profile')
+
+        });
     });
+
+    describe('Negative Tests', function () {
+
+        it('Sign in with invalid credentials', function () {
+
+            cy.visit('/user/login')
+
+            cy.get('#normal_login_email')
+                .type('dumpdusty2@gmail.com')
+            cy.get('#normal_login_password')
+                .type('test')
+            cy.get('.login-form-button')
+                .click()
+
+
+            cy.get('.ant-notification-notice-message')
+                .should('have.text', 'Auth failed')
+        });
+    })
 
 })
